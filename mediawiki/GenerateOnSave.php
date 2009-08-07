36ed307d469e5c4b5be01824a433017e34fa25d7
Staffan Malmgren <staffan.malmgren@gmail.com>
2009-08-07 12:56:56 +0000

    misc improvements, still a lot of cruft

--- mediawiki/GenerateOnSave.php
+++ mediawiki/GenerateOnSave.php
@@ -1,33 +1,24 @@
 <?php
+define("FERENDA_DIR",'/www/staffan/ferenda.lagen.nu');
+define("PYTHON", "/www/staffan/ferenda.lagen.nu/bin/python");
+
 $wgHooks['ArticleSaveComplete'][] = 'fnGenerateOnSave';
 function fnGenerateOnSave(&$article, &$user, &$text, &$summary, &$minoredit, 
                         &$watchthis, &$sectionanchor, &$flags, &$revision){
 
-   $parts = explode("/", $article->getTitle());
-   if (count($parts) < 2) {
-       return true;
-   }
-   $type = $parts[0];
-   $basefile = $parts[1];
+  if ($minoredit == true) {
+    // No generate for minor edits
+    return true;
+  }
 
-   switch($type) {
-   case "sfs":
-       $module = "SFS.py";
-       $basefile = str_replace(":","/",$basefile);
-       break;
-   case "dom":
-       $module = "DV.py";
-       // fixme: find out correct basefile ("nja/2004s43" => "HDO/B123-04")
-       break;
-   default:
-       return true;
-   }
-   $cmd = sprintf("PYTHON_EGG_CACHE=/tmp /usr/local/staffan/bin/python /www/staffan/ferenda.lagen.nu/%s Generate %s &> /tmp/GenerateOnSave.txt", $module, escapeshellarg($basefile));
-   $line = system($cmd, &$return_var);
+  $article = escapeshellarg($article->getTitle());
+  $cmd = sprintf("PYTHON_EGG_CACHE=/tmp LC_CTYPE=sv_SE.iso88591 %s %s/Manager.py WikiUpdate %s &> /tmp/GenerateOnSave.txt", PYTHON, FERENDA_DIR, $article);
+   $line = system($cmd, $return_var);
    if ($return_var != 0) {
       $fh = fopen("/tmp/GenerateOnSave.txt", 'r');
       $err = fread($fh,filesize("/tmp/GenerateOnSave.txt"));
-      throw new Exception("<pre>".$cmd."\n".$err);
+      mail("staffan@tomtebo.org", "GenerateOnSave error for ".$article, $cmd."\n\n\n".$err);
+      throw new Exception($cmd."\n\n".$err);
    }
    return true;
 }
